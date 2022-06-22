1f8413b11a8c447c0fee62268056f4635126aad9
LeVien <levien.workspace@gmail.com>
2022-06-22 13:37:28 +0700

    chore: delete mint

--- packages/example-mint-nft/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 Fewcha. All rights reserved.
-
-import React from "react";
-
-const Home: React.FC = () => {
-  return <>Fewcha DApp Example</>;
-};
-
-export default Home;
--- packages/example-mint-nft/src/services/cn.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 Fewcha. All rights reserved.
-
-import classnames from "@sindresorhus/class-names";
-
-const cn = classnames;
-
-export type ClassNames = string | [] | {};
-
-export default cn;
