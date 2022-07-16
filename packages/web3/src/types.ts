85f6553cac67e19a6e3f92186352dbd17615b9d2
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2022-07-16 18:14:38 +0700

    improve types for @fewcha/web3

--- packages/web3/src/types.ts
+++ packages/web3/src/types.ts
@@ -33,37 +33,40 @@ export interface Web3ProviderType {
   getNetwork(): Promise<string>;
   getBalance(): Promise<string>;
 
-  sdk: {
-    getAccount(accountAddress: MaybeHexString): Promise<Account>;
-    getAccountTransactions(accountAddress: MaybeHexString, query?: { start?: number; limit?: number }): Promise<OnChainTransaction[]>;
-    getAccountModules(accountAddress: MaybeHexString, query?: { version?: LedgerVersion }): Promise<MoveModule[]>;
-    getAccountModule(accountAddress: MaybeHexString, moduleName: string, query?: { version?: LedgerVersion }): Promise<MoveModule>;
-    getAccountResources(accountAddress: MaybeHexString, query?: { version?: LedgerVersion }): Promise<AccountResource[]>;
-    getAccountResource(accountAddress: MaybeHexString, resourceType: string, query?: { version?: LedgerVersion }): Promise<AccountResource>;
-
-    getEventsByEventKey(eventKey: HexEncodedBytes): Promise<Event[]>;
-    getEventsByEventHandle(address: MaybeHexString, eventHandleStruct: MoveStructTagId, fieldName: string, query?: { start?: number; limit?: number }): Promise<Event[]>;
-
-    getTransactions(query?: { start?: number; limit?: number }): Promise<OnChainTransaction[]>;
-    getTransaction(txnHashOrVersion: string): Promise<Transaction>;
-
-    transactionPending(txnHash: HexEncodedBytes): Promise<boolean>;
-    waitForTransaction(txnHash: HexEncodedBytes): Promise<void>;
-    getLedgerInfo(params: RequestParams): Promise<LedgerInfo>;
-    getChainId(params: RequestParams): Promise<number>;
-    getTableItem(handle: string, data: TableItemRequest, params?: RequestParams): Promise<any>;
-  };
-
-  token: {
-    createCollection(name: string, description: string, uri: string): Promise<string>;
-    createToken(collectionName: string, name: string, description: string, supply: number, uri: string, royalty_points_per_million: number): Promise<string>;
-    offerToken(receiver: MaybeHexString, creator: MaybeHexString, collectionName: string, name: string, amount: number): Promise<HexEncodedBytes>;
-    claimToken(sender: MaybeHexString, creator: MaybeHexString, collectionName: string, name: string): Promise<HexEncodedBytes>;
-    cancelTokenOffer(receiver: MaybeHexString, creator: MaybeHexString, collectionName: string, name: string): Promise<HexEncodedBytes>;
-
-    getCollectionData(creator: MaybeHexString, collectionName: string): Promise<any>;
-    getTokenData(creator: MaybeHexString, collectionName: string, tokenName: string): Promise<TokenData>;
-    getTokenBalance(creator: MaybeHexString, collectionName: string, tokenName: string): Promise<Token>;
-    getTokenBalanceForAccount(account: MaybeHexString, tokenId: TokenId): Promise<Token>;
-  };
+  sdk: Web3SDK;
+  token: Web3Token;
 }
+
+export type Web3SDK = {
+  getAccount(accountAddress: MaybeHexString): Promise<Account>;
+  getAccountTransactions(accountAddress: MaybeHexString, query?: { start?: number; limit?: number }): Promise<OnChainTransaction[]>;
+  getAccountModules(accountAddress: MaybeHexString, query?: { version?: LedgerVersion }): Promise<MoveModule[]>;
+  getAccountModule(accountAddress: MaybeHexString, moduleName: string, query?: { version?: LedgerVersion }): Promise<MoveModule>;
+  getAccountResources(accountAddress: MaybeHexString, query?: { version?: LedgerVersion }): Promise<AccountResource[]>;
+  getAccountResource(accountAddress: MaybeHexString, resourceType: string, query?: { version?: LedgerVersion }): Promise<AccountResource>;
+
+  getEventsByEventKey(eventKey: HexEncodedBytes): Promise<Event[]>;
+  getEventsByEventHandle(address: MaybeHexString, eventHandleStruct: MoveStructTagId, fieldName: string, query?: { start?: number; limit?: number }): Promise<Event[]>;
+
+  getTransactions(query?: { start?: number; limit?: number }): Promise<OnChainTransaction[]>;
+  getTransaction(txnHashOrVersion: string): Promise<Transaction>;
+
+  transactionPending(txnHash: HexEncodedBytes): Promise<boolean>;
+  waitForTransaction(txnHash: HexEncodedBytes): Promise<void>;
+  getLedgerInfo(params: RequestParams): Promise<LedgerInfo>;
+  getChainId(params: RequestParams): Promise<number>;
+  getTableItem(handle: string, data: TableItemRequest, params?: RequestParams): Promise<any>;
+};
+
+export type Web3Token = {
+  createCollection(name: string, description: string, uri: string): Promise<string>;
+  createToken(collectionName: string, name: string, description: string, supply: number, uri: string, royalty_points_per_million: number): Promise<string>;
+  offerToken(receiver: MaybeHexString, creator: MaybeHexString, collectionName: string, name: string, amount: number): Promise<HexEncodedBytes>;
+  claimToken(sender: MaybeHexString, creator: MaybeHexString, collectionName: string, name: string): Promise<HexEncodedBytes>;
+  cancelTokenOffer(receiver: MaybeHexString, creator: MaybeHexString, collectionName: string, name: string): Promise<HexEncodedBytes>;
+
+  getCollectionData(creator: MaybeHexString, collectionName: string): Promise<any>;
+  getTokenData(creator: MaybeHexString, collectionName: string, tokenName: string): Promise<TokenData>;
+  getTokenBalance(creator: MaybeHexString, collectionName: string, tokenName: string): Promise<Token>;
+  getTokenBalanceForAccount(account: MaybeHexString, tokenId: TokenId): Promise<Token>;
+};
