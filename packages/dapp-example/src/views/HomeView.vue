0e7c9e690c4c2809e347d7acbfdcde259299ee22
ChungNguyen97 <nguyenchungdev@gmail.com>
2022-08-08 15:42:44 +0700

    add dapp-ex

---
+++ packages/dapp-example/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h3 class="intro">Wellcome Fewcha Wallet</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped>
+.intro {
+  text-align: center;
+  margin-top: 128px;
+  color: chocolate;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+</style>
