9c8745ec3c41950433ff56cda2492751baa0482c
yvvw <15168529+yvvw@users.noreply.github.com>
2022-08-11 11:10:08 +0800

    fix next.js render failed in ssr mode.

--- packages/nextjs-web3-react/components/Layout.tsx
+++ packages/nextjs-web3-react/components/Layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import Web3Provider from "@fewcha/web3-react";
+import dynamic from "next/dynamic";
+
+// https://nextjs.org/docs/advanced-features/dynamic-import#with-no-ssr
+const Web3Provider = dynamic(() => import("@fewcha/web3-react"), { ssr: false });
 
 const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   return <Web3Provider>{children}</Web3Provider>;
