38a8e30575c5567acc9349fc8322986302b22315
HoaLe <lvhoa58@gmail.com>
2023-07-19 12:48:51 +0700

    chore: bump sdk version, v0.1.48

--- packages/aptos/examples/typescript-esm/index.ts
+++ packages/aptos/examples/typescript-esm/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { AptosClient, AptosAccount, FaucetClient, BCS, TxnBuilderTypes } from "aptos";
+import { AptosClient, AptosAccount, FaucetClient, BCS, TxnBuilderTypes, Provider, Network, TokenClient } from "aptos";
 import assert from "assert";
 
 const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
@@ -23,37 +23,29 @@ const {
 (async () => {
   const client = new AptosClient(NODE_URL);
   const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
+  const tokenClient = new TokenClient(client);
 
   // Generates key pair for a new account
   const account1 = new AptosAccount();
-  await faucetClient.fundAccount(account1.address(), 100000);
+  await faucetClient.fundAccount(account1.address(), 100_000_000);
   let resources = await client.getAccountResources(account1.address());
   let accountResource = resources.find((r: any) => r.type === aptosCoinStore);
   let balance = parseInt((accountResource?.data as any).coin.value);
-  assert(balance === 100000);
-  console.log(`account2 coins: ${balance}. Should be 100000!`);
+  assert(balance === 100_000_000);
+  console.log(`account1 coins: ${balance}. Should be 100_000_000!`);
 
   const account2 = new AptosAccount();
-  // Creates the second account and fund the account with 0 AptosCoin
-  await faucetClient.fundAccount(account2.address(), 0);
-  resources = await client.getAccountResources(account2.address());
-  accountResource = resources.find((r: any) => r.type === aptosCoinStore);
-  balance = parseInt((accountResource?.data as any).coin.value);
-  assert(balance === 0);
-  console.log(`account2 coins: ${balance}. Should be 0!`);
-
-  const token = new TypeTagStruct(StructTag.fromString("0x1::aptos_coin::AptosCoin"));
 
   // TS SDK support 3 types of transaction payloads: `EntryFunction`, `Script` and `Module`.
   // See https://aptos-labs.github.io/ts-sdk-doc/ for the details.
   const entryFunctionPayload = new TransactionPayloadEntryFunction(
     EntryFunction.natural(
       // Fully qualified module name, `AccountAddress::ModuleName`
-      "0x1::coin",
+      "0x1::aptos_account",
       // Module function
       "transfer",
       // The coin type to transfer
-      [token],
+      [],
       // Arguments for function `transfer`: receiver account address and amount to transfer
       [BCS.bcsToBytes(AccountAddress.fromHex(account2.address())), BCS.bcsSerializeUint64(717)],
     ),
@@ -72,9 +64,9 @@ const {
     BigInt(sequenceNumber),
     entryFunctionPayload,
     // Max gas unit to spend
-    BigInt(2000),
+    BigInt(10000),
     // Gas price per unit
-    BigInt(1),
+    BigInt(100),
     // Expiration timestamp. Transaction is discarded if it is not executed within 10 seconds from now.
     BigInt(Math.floor(Date.now() / 1000) + 10),
     new ChainId(chainId),
@@ -92,4 +84,47 @@ const {
   balance = parseInt((accountResource?.data as any).coin.value);
   assert(balance === 717);
   console.log(`account2 coins: ${balance}. Should be 717!`);
+
+  const provider = new Provider(Network.DEVNET);
+  console.log("\n=== Checking if indexer devnet chainId same as fullnode chainId  ===");
+  const indexerLedgerInfo = await provider.getIndexerLedgerInfo();
+  const fullNodeChainId = await provider.getChainId();
+
+  console.log(
+    `\n fullnode chain id is: ${fullNodeChainId}, indexer chain id is: ${indexerLedgerInfo.ledger_infos[0].chain_id}`,
+  );
+  if (indexerLedgerInfo.ledger_infos[0].chain_id !== fullNodeChainId) {
+    console.log(`\n fullnode chain id and indexer chain id are not synced, skipping rest of tests`);
+    return;
+  }
+
+  console.log("=== Creating account1's NFT Collection and Token ===");
+
+  const collectionName = "Alice's";
+  const tokenName = "Alice's first token";
+
+  // Create the collection.
+  // :!:>section_4
+  const txnHash1 = await tokenClient.createCollection(
+    account1,
+    collectionName,
+    "Alice's simple collection",
+    "https://alice.com",
+  ); // <:!:section_4
+  await client.waitForTransaction(txnHash1, { checkSuccess: true });
+
+  // Create a token in that collection.
+  // :!:>section_5
+  const txnHash2 = await tokenClient.createToken(
+    account1,
+    collectionName,
+    tokenName,
+    "Alice's simple token",
+    1,
+    "https://aptos.dev/img/nyan.jpeg",
+  ); // <:!:section_5
+  await client.waitForTransaction(txnHash2, { checkSuccess: true });
+
+  const nfts = await provider.getAccountNFTs(account1.address().hex());
+  console.log(`account1 current token ownership: ${nfts.current_token_ownerships[0].amount}. Should be 1`);
 })();
